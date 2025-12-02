fa3ff754c281770eb5c0754fed8ff00b3a3dc0d2
rmdelbarrio <rmdelbarrio@gbox.ncf.edu.ph>
2025-12-02 19:14:59 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
-//added appcontroller and made a .ts file for it
-
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 import { PositionModule } from './position/position.module';
+import { DatabaseModule } from './database/database.module';
+// FIX: Import the new Dashboard module
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule,PositionModule],
-
+  imports: [
+    AuthModule, 
+    UsersModule, 
+    PositionModule, 
+    DatabaseModule,
+    DashboardModule, // FIX: Add the Dashboard module here
+  ],
   controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
