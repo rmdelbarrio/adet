270157165e684af1bce4b7ba71a5d61b1a2ea2c7
rmdelbarrio <rmdelbarrio@gbox.ncf.edu.ph>
2025-12-02 19:05:16 +0700

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service'; 
-import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
+import { Reflector } from '@nestjs/core'; // Import Reflector
 
 @Module({
   imports: [
     UsersModule,
-    PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_ACCESS_TOKEN_SECRET || 'access_secret',
-      signOptions: { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN || '900s' },
+      secret: process.env.JWT_SECRET || 'secret_key',
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '60s' },
     }),
   ],
-  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  // FIX: Add Reflector and JwtStrategy to providers/exports
+  providers: [AuthService, JwtStrategy, Reflector],
+  exports: [AuthService, JwtModule, Reflector],
 })
 export class AuthModule {}
